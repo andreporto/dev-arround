e5fa35114fb68bcb9a463f0fe718cce85f3aa48e
André Porto <andreporto@yahoo.com>
2020-01-15 13:41:33 -0300

    add react front-end

--- back-end/src/index.js
+++ back-end/src/index.js
@@ -2,8 +2,11 @@ require('dotenv/config');
 const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 mongoose.set('useCreateIndex', true);
 mongoose.connect(process.env.MONGO_URI,{
